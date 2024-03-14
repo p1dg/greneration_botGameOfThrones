e4440f85faa3466aca522301ed3a16810bd60e0c
p1dg <poshaprogrammer@mail.ru>
2024-03-15 01:40:03 +0300

    async

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from data.data_preparation import parse_dataset
 from model.train_llama import train_llama_model
 from model.inference_chat_bot import GenerationBot
+import asyncio
 
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder="templates")
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 messages = []
 bot = None
 
@@ -15,18 +17,18 @@ def index():
 
 
 @app.route("/send_message", methods=["POST"])
-def send_message():
-    input = request.json
+async def send_message():
+    input = request.get_json()
     user_message = input["message"]
     if len(user_message) != 0:
         messages.append(f"you: {user_message}")
-        answer, role = bot.generate_answer(query=user_message)
+        answer, role = await asyncio.to_thread(bot.generate_answer, query=user_message)
         messages.append(f"{role}: {answer}")
     return jsonify({"message": user_message})
 
 
 @app.route("/get_messages", methods=["GET"])
-def get_messages():
+async def get_messages():
     return jsonify({"messages": messages})
 
 
@@ -41,5 +43,4 @@ if __name__ == "__main__":
         trained_model_dir='./models_storage/checkpoint-700',
         role='daenerys targaryen',
     )
-    answer, role = bot.generate_answer('whhat happend to ned stark?')
-    app.run(debug=True)
+    app.run()
